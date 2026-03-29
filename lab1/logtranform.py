91c8fd7a5e2ef9c2d2f8ebd3c1c47a745c2401d4
Angel <angelkhatriii7@gmail.com>
2026-03-29 09:09:22 +0545

    to transform image from dark to high intensity

---
+++ lab1/logtranform.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+img=cv2.imread("./images/lena.jpg")
+print(img.shape)
+c=255/(np.log(1+np.max(img)))
+log_image=c*(np.log(1+img))
+log_image=np.array(log_image,dtype=np.uint8)
+cv2.imshow("window",log_image)
+cv2.waitKey(0)
